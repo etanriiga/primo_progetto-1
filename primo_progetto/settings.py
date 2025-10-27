e087ef24b174449c2567b7f84833c3832a0aee57
Elsehmawi <thomaselsehmawi06@gmail.com>
2025-10-27 11:42:09 +0100

    Update settings.py
    aggiornato parametro allowed host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-unomnf-891g*$hg3e!)ldz(seqw)spd5=tc$(-sg1h(+g%(s@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
