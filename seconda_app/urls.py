b37a7fec79436745e53655f4ada359e99ecab53f
elsehmawi <thomaselsehmawi06@gmail.com>
2025-11-10 11:06:06 +0100

    pass12real

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -4,6 +4,6 @@ app_name="seconda_app"
 urlpatterns=[
     path('es_if',es_if,name='es_if'),
     path('if_else_elif',if_else_elif,name='if_else_elif'),
-     path(' es_for', es_for,name=' es_for'),
+    path(' es_for', es_for,name=' es_for'),
 
 ]
