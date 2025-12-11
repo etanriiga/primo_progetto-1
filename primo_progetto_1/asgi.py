aff231da008c25e90117c1cf1123daa0bfdce4cc
Elsehmawi <thomaselsehmawi06@gmail.com>
2025-12-11 12:55:53 +0100

    modifica nome progetto

--- primo_progetto_1/asgi.py
+++ primo_progetto_1/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primo_progetto-1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primo_progetto_1.settings')
 
 application = get_asgi_application()
--- primo_progetto_1/wsgi.py
+++ primo_progetto_1/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primo_progetto-1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primo_progetto_1.settings')
 
 application = get_wsgi_application()
